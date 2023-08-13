9101dbefff32c6990bb3fa4d303401f5caf43e3c
Andrew Choi <a.choi@smartsoltech.kr>
2023-08-13 13:15:44 +0900

    Version 2. KeyWords moved to keywords.yaml file

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import yaml
+
+
+
 
 class LetterCategorizer:
     def __init__(self, input_path, output_path):
@@ -6,17 +10,13 @@ class LetterCategorizer:
         self.output_path = output_path
         self.data = None
         
+        
         # Словарь с ключевыми словами для каждой категории
-        self.keywords_dict = {
-            "Security": ["безопасность", "пароль", "взлом", "защита", "подозрительная активность", "код доступа"],
-            "Refunds": ["возврат", "возмещение", "отмена", "деньги", "средства", "заказ"],
-            "Troubleshooting": ["проблема", "не работает", "ошибка", "падение", "сбой", "зависает"],
-            "Account": ["аккаунт", "профиль", "регистрация", "вход", "выход", "доступ"],
-            "Advertising and Collaboration": ["реклама", "сотрудничество", "партнерство", "бренд", "продвижение"],
-            "Limits": ["лимит", "ограничение", "предел"],
-            "Payments": ["оплата", "платеж", "счет", "банковская карта", "транзакция"],
-            "Features": ["функционал", "возможность", "функция", "новинка", "опция"]
-        }
+        with open('keyword.yaml', 'r', encoding='UTF-8') as tmp_data:
+            data = yaml.safe_load(tmp_data)
+            
+        self.keywords_dict = data['keywords']
+        print(data['keywords'])
 
     def _categorize_letter(self, letter):
         """Категоризация письма на основе ключевых слов."""
